ebc3f2d1ae2b3f41c4173c1a2cea55ad3c10deac
Ivan <ivan.tixonov00@mail.ru>
2025-08-27 13:18:20 +0700

    Анимация кнопок перехода

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Route, Routes} from "react-router-dom";
 import Home from "./components/Home.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import ShowMenuItem from "./components/ShowMenuItem.jsx";
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
             <main>
                 <Routes>
                     <Route path="/" element={<Home/>}/>
+                    <Route path="/show" element={<ShowMenuItem/>}/>
                 </Routes>
             </main>
 
--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,8 +1,34 @@
 import play from '../img/icons/play.svg';
+import {useNavigate} from "react-router-dom";
+import {useEffect, useState} from "react";
 
 export default function MenuItem({img, title, bgImg}) {
+    const navigate = useNavigate();
+
+    const [isActive, setIsActive] = useState(false);
+    const [isAnimate, setIsAnimate] = useState(false);
+
+    const handleClick = () => {
+        setIsActive(prev => !prev);
+
+        const timeout = setTimeout(() =>
+                navigate('/show'),
+            1000);
+
+        return () => clearTimeout(timeout);
+    };
+
+    useEffect(() => {
+        const interval = setInterval(() =>
+                setIsAnimate(prev => !prev),
+            5000);
+
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <div className="menu-item" style={{background: `url(${bgImg}) no-repeat center / cover`}}>
+        <div className={`menu-item ${isActive ? 'active' : ''}`}
+             style={{background: `url(${bgImg}) no-repeat center / cover`}}>
             <div className="top">
                 <div className="icon">
                     <img src={img} alt="Иконка" loading="lazy"/>
@@ -12,9 +38,9 @@ export default function MenuItem({img, title, bgImg}) {
             <div className="bottom">
                 <h6>{title}</h6>
 
-                <div className="play">
-                    <img src={play} alt="Вперед"/>
-                </div>
+                <button type="button" className={`play ${isAnimate ? 'animate' : ''}`} onClick={handleClick}>
+                    <img src={play} alt="Вперед" className={isAnimate ? 'animate' : ''}/>
+                </button>
             </div>
         </div>
     )
