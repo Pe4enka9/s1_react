3231dd10b213fc1475bcc72f29b8b87f2e9fe9d8
Pe4enka9 <ivan.tixonov000@gmail.com>
2026-06-28 23:09:36 +0700

    цвета логотипа поменены местами

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -7,8 +7,8 @@ export default function Logo() {
             aria-label="На главную страницу"
             className="flex items-baseline select-none"
         >
-            <span className="font-bold text-3xl text-primary">S</span>
-            <span className="font-bold text-3xl text-white">1</span>
+            <span className="font-bold text-3xl text-white">S</span>
+            <span className="font-bold text-3xl text-primary">1</span>
         </Link>
     );
 }
